a8b6e39c0c19292b512f7b8bb823a173d50aa436
arojmar <arojmar@gmail.com>
2021-03-28 14:50:29 +0200

    Added Integration with ElasticSearch and Kibana

---
+++ src/test/java/com/alfonsoframework/app/util/reporting/ScenarioExecutionInfo.java
@@ -0,0 +1,13 @@
+package com.alfonsoframework.app.util.reporting;
+
+import lombok.Data;
+
+@Data
+public class ScenarioExecutionInfo {
+
+    private String status;
+    private String id;
+    private String name;
+    private String executionId;
+
+}
