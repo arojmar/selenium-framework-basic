55f5baa056411879506f26de44ff54b6b7cd53af
alfonso <arojmar@gmail.com>
2021-03-31 18:39:09 +0200

    Commented reporting throw Kibana and added jenkinsfile

--- src/test/java/CucumberTest.java
+++ src/test/java/CucumberTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
         plugin = { "de.monochromata.cucumber.report.PrettyReports:target/cucumber",
-                "pretty", "json:target/cucumber-reports/Cucumber.json",
-                "com.alfonsoframework.app.util.reporting.ReportListener"})
+                "pretty", "json:target/cucumber-reports/Cucumber.json"})
+//                "com.alfonsoframework.app.util.reporting.ReportListener"})
 public class CucumberTest {
 }
